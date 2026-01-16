4a92ed7d000c2b9d5758140fd4027b670e4dcb9e
Qian Wu <qian.wu@gomotive.com>
2026-01-15 21:48:50 -0500

    Fix JSX syntax error in home-page

--- src/app/components/home-page.tsx
+++ src/app/components/home-page.tsx
@@ -56,7 +56,6 @@ export function HomePage({ onSignIn }: HomePageProps) {
               </div>
             </div>
             */}
-            </div>
             <div className="flex flex-col sm:flex-row gap-4 justify-center">
               <Button onClick={onSignIn} size="lg" className="text-lg px-8 py-6 bg-indigo-600 hover:bg-indigo-700">
                 Get Started <ArrowRight className="ml-2 w-5 h-5" />
